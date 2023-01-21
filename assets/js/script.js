7158aeb3d5293c04d02cb2163a95ff1cd65648c9
Travis <traviscmiller01@gmail.com>
2023-01-21 15:44:06 -0800

    finished logic for getParameters function. function returns object with values received by user windwo inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,48 @@ var numericalCharacters = [0,1,2,3,4,5,6,7,8,9];
 // Write password to the #password input
 function writePassword() {
   //password set to result of generatePassword Function
-  var password = generatePassword();
+  var password = getParameters();
   //selects textbox using DOM manipulation
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
 
 }
-
+//Creates randomly generated string using parameters provided by user to be used for passwords.
 function generatePassword(){
-  
+  var parameters = getParameters();
+
 }
 
 function getParameters(){
-  var length = prompt('Enter the desired length of your password (8-128)', 10);
-  var hasSpecial = confirm('Include Special Characters?');
-  var hasLower = confirm('Include lower case characters?');
-  var hasUpper = confirm('Include Upper Case Characters?');
-  var hasNumber = confirm('Include Numbers?');
+  var length = parseInt(prompt('Enter the desired length of your password 8-128'));
+  
+    //makes sure length entered by user is greater than 8
+    if(length < 8){
+      alert("Entry was less than 8. Please provide a number 8-128.");
+      return null;
+      //makes sure length entered by user is less than 128
+    }else if(length > 128){
+      alert("Entry was greater than 128. Please provide a number 8-128.");
+      return null;
+    //makes sure length entered by user is a number and not a string
+    }else if(Number.isNaN(length)){
+      alert('Entry was not a number. Please provide a number 8-128.');
+      return null;
+    }
+  var hasSpecial = confirm('Include Special Characters? Press OK.');
+  var hasLower = confirm('Include lower case characters? Press OK.');
+  var hasUpper = confirm('Include Upper Case Characters? Press OK.');
+  var hasNumber = confirm('Include Numbers? Press OK.');
+
+  var passwordParameter = {
+    hasSpecial:hasSpecial,
+    hasLower:hasLower,
+    hasUpper:hasUpper,
+    hasNumber:hasNumber
+  };
+
+  return passwordParameter;
 }
 
 // Add event listener to generate button
